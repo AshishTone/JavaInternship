d3a31d70cb664f2a5ce633e0894cdc5d2d1d54c3
AshishTone <ashishtone123@gmail.com>
2025-12-12 12:24:39 +0530

    Adding Assignment 01

--- Arithmatic/ArithmaticOperations/src/com/arithmatic/Main.java
+++
@@ -1,9 +0,0 @@
-package com.arithmatic;
-
-public class Main {
-	public static void main(String args[]) {
-		arithmatic ath = new arithmatic();
-		System.out.println("Perimeter of 23x20 rectangle: " + ath.CalPeriRect(23, 20));
-	}
-
-}
--- Hi.java
+++
@@ -1,8 +0,0 @@
-package com.Hi;
-
-public class Hi {
-	public static void main(String a[]) {
-		System.out.println("Hello World!");
-		//hi hello
-	}
-}
